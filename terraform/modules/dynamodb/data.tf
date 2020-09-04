8d7dd52e140669122395aeb5cd38e6b4eff7fbc4
danbeckwith <danbeckwithh@gmail.com>
2020-09-04 12:14:41 +0100

    Adding example object to dynamo

---
+++ terraform/modules/dynamodb/data.tf
@@ -0,0 +1,45 @@
+resource "aws_dynamodb_table_item" "example_user" {
+  table_name = aws_dynamodb_table.user_streams.name
+  hash_key   = aws_dynamodb_table.user_streams.hash_key
+
+  item = <<ITEM
+{
+    "UserId": {
+        "S": "388483ieowjds"
+    },
+    "Name": {
+        "S": "User 123"
+    },
+    "Streams": {
+        "L": [
+            {
+                "M": {
+                    "StreamId": {
+                        "S": "1927793812"
+                    },
+                    "Title": {
+                        "S": "Man U v Chelsea"
+                    },
+                    "Finish": {
+                        "S": "2020-10-02T11:45:00Z"
+                    }
+                }
+            },
+            {
+                "M": {
+                    "StreamId": {
+                        "S": "08e2joik"
+                    },
+                    "Title": {
+                        "S": "The Ashes"
+                    },
+                    "Finish": {
+                        "S": "2020-10-02T13:45:00Z"
+                    }
+                }
+            }
+        ]
+    }
+}
+ITEM
+}
